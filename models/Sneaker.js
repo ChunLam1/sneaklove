4382ff42bd0393e6b8dcedf39583c293f0af4067
Chun Lam <Chun01@hotmail.fr>
2022-01-13 16:38:44 +0100

    odelsAdded

---
+++ models/Sneaker.js
@@ -0,0 +1,17 @@
+const { model, Schema } = require("mongoose");
+
+const SneakerSchema = new Schema({
+    name: String,
+    ref: String,
+    size: Number,
+    description: String,
+    price: Number,
+    category: {type:String, enum:{men, women, kids}},
+    id_tags: [{type: Schema.Types.ObjectId, ref: 'Tags'}]
+});
+
+const SneakerModel = model("sneaker", SneakerSchema);
+
+module.exports = SneakerModel;
+
+    
